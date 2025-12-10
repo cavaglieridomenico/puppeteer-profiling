32600e1bd771c5622b92166ecc34f3225fcf8bd5
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2025-12-10 12:01:07 +0100

    add logic for url handling

--- navigate.js
+++ navigate.js
@@ -1,10 +1,14 @@
 const http = require('http');
-const url = process.argv[2];
+const { urls } = require('./src/urls.js');
 
-if (!url) {
-  console.error('Please provide a URL as an argument.');
+const urlArg = process.argv[2];
+
+if (!urlArg) {
+  console.error('Please provide a URL or a URL alias as an argument.');
   process.exit(1);
 }
+
+const url = urls[urlArg] || urlArg;
 
 const req = http.get(`http://localhost:8080/navigate:url?url=${encodeURIComponent(url)}`, (res) => {
   let data = '';
---
+++ src/urls.js
@@ -0,0 +1,6 @@
+const urls = {
+  TV20: 'https://vmmv-uat.luxottica.com/v/5.5.2/demo/demo.html?key=95702A5E-1523-4ADD-AC16-5CE7062F7E31',
+  TV18: 'https://www.oakley.com/en-us/product/W0OO9406?variant=888392404756',
+};
+
+module.exports = { urls };
