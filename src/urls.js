79289648d1d6df66a903c1251c2235364eb1d5e6
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2025-12-11 10:53:38 +0100

    add further urls

--- src/urls.js
+++ src/urls.js
@@ -4,6 +4,8 @@ const urls = {
     'https://vmmv-uat.luxottica.com/v/5.5.2/demo/demo.html?key=95702A5E-1523-4ADD-AC16-5CE7062F7E31',
   VMMV_TV18:
     'https://www.oakley.com/en-us/product/W0OO9406?variant=888392404756',
+  VMCORE_VMP_TV26:
+    'https://vmcore-test.luxottica.com/demo/VMP-UI/feature/2.1.3_baseline/index.html#/',
   VMCORE_VMP_TV27_02:
     'https://vmcore-test.luxottica.com/demo/VMP-UI/feature/PICOMMON-4305-test-faceweb-reset/index.html#/',
   PDW_BYPASS: 'https://pdweb.luxottica.com/luxotticabypass',
